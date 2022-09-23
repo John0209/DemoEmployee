d483fbf0b3b8a523f165bb3f5dcaceaa8ea66709
Tuấn Vũ <113121915+John0209@users.noreply.github.com>
2022-09-23 09:02:08 +0700

    note

---
+++ DemoEmployee/DemoEmployee/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoEmployee.Per;
+
+public abstract class Person
+{
+    private int _id;
+    private string _name;
+    private DateTime _dob;
+    public int Id { get { return _id; } set { _id = value; } }
+    public string Name { get { return _name; } set { _name = value; } }
+    public DateTime Dob { get { return _dob; } set { _dob = value; } }
+    public string Address { get; set; }
+    public abstract double getSalary();
+    public virtual string Display() => $"ID: {Id}, Name: {Name}, Dob: {Dob}";
+    
+
+}
