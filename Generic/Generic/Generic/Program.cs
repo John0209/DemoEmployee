fa1f2defb285c624a531bba15952b06ff5714f55
Tuấn Vũ <113121915+John0209@users.noreply.github.com>
2022-09-27 10:56:16 +0700

    push

---
+++ Generic/Generic/Generic/Program.cs
@@ -0,0 +1,31 @@
+﻿using DemoEmployee.Employees;
+using DemoEmployee.Manager;
+namespace DemoEmployee.program;
+public class Program
+{
+    public static void Main()
+    {
+        Employee[] employee = new Employee[]
+        {
+            new Employee(1,"Hùng",new DateTime(1989,12,2),"HCM",3500,30),
+            new Employee(2,"Hào",new DateTime(1929,10,11),"HCM",3500,12),
+            new Employee(3,"Chiến",new DateTime(1999,1,27),"HCM",3500,22),
+        };
+        EmployeeManager empManager= new EmployeeManager(employee);
+        //Add
+        Employee e = new Employee();
+        e.Id = 4;
+        e.Name = "Huy";
+        e.Manday = 10;
+        e.RateSalary = 3500;
+        e.Address = "HN";
+        e.Dob = new DateTime(2000, 2, 18);
+       
+        empManager.Add(e);
+
+        //print all
+        Console.WriteLine("===================");
+        empManager.Display();
+        Console.ReadKey();
+    }
+}
