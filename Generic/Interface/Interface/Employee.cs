e3ad7ab2681de3c2fb6f0dd07ff04f1bd5a68bc8
Tuấn Vũ <113121915+John0209@users.noreply.github.com>
2022-09-27 08:30:07 +0700

    update generic

---
+++ Generic/Interface/Interface/Employee.cs
@@ -0,0 +1,60 @@
+﻿using DemoEmployee.Per;
+
+namespace DemoEmployee.Employees;
+public class Employee:Person
+{
+//    //field 
+//    private int _id;
+//    private string _name;
+//    private DateTime _dob;
+
+//    //properties
+//    public int Id
+//    {
+//        get { return _id; }
+//        set { _id = value; }
+//    }
+//    public string Name
+//    {
+//        get => _name;
+//        set => _name = value;
+//    }
+//    public DateTime Dob
+//    {
+//        get => _dob;
+//        set => _dob = value;
+//    }
+    public string Address { get; set; }
+    public double RateSalary { get;set; }
+    public int Manday { get;set; }
+    //constructors
+    public Employee()
+    {
+    }
+
+    public Employee(int id, string name, DateTime dob,
+        string address, double rateSalary, int manday)
+    {
+        Id = id;
+        Name = name;
+        Dob = dob;
+        Address = address;
+        RateSalary = rateSalary;
+        Manday = manday;
+    }
+    //methods
+    private double _calcSalary() { return RateSalary * Manday; }
+
+    public override double getSalary()
+    {
+       return this.Manday * this.RateSalary;
+    }
+
+    public void Print() => Console.WriteLine("Print a Employee");
+
+    public override string Display()
+    => base.Display() + $" Salary: {getSalary()}";
+
+    public override string? ToString()
+    => base.ToString() + $" Salary: {getSalary()}";
+}
---
+++ Generic/Interface/Interface/IPerson.cs
@@ -0,0 +1,19 @@
+﻿using DemoEmployee.Per;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoEmployee.Repository;
+
+public interface IPerson
+{
+    Person[] GetAll();
+    void Display();
+    void Add(Person p);
+    void Update(Person p);  
+    Person GetbyId(int id);
+    void Delete(Person p);
+
+}
