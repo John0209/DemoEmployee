5f6cdc6b1ab7f4e8a8592fd4a1e45d58b495af7e
Tuấn Vũ <113121915+John0209@users.noreply.github.com>
2022-09-30 21:20:50 +0700

    update

--- Delegate/DelegateDemo/DelegateDemo/delegate.cs
+++ Delegate/DelegateDemo/DelegateDemo/delegate.cs
@@ -8,9 +8,43 @@ namespace DelegateDemo;
 
 class Demo
 {
-    delegate int MyDelegate(string s);
-    static void Main(string[] args)
+    delegate string MyDelegate(string s);
+    public static void Main(string[] args)
     {
+        MyDelegate john = new MyDelegate(myFun);
+        MyDelegate jinx = new MyDelegate(myFoam);
+        MyDelegate multicast= john+jinx;
+        string s = "Welcome";
+        //delegate 1 function
+        Console.WriteLine("---delegate 1 function---");
+        john(s);
+        Console.WriteLine("---delegate 2 function---");
+        multicast(s);
+        Console.WriteLine("---delegate - function---");
+        multicast -= john;
+        multicast(s);
+        Console.WriteLine("**************");
+        myName(jinx);
+        Console.ReadLine();
+    }
+    static void myName(MyDelegate print)
+    {
+        Console.Write("Enter Name: ");
+        string name = Console.ReadLine();
+        print(name);
+    }
+    static string myFun(string s)
+    {
+        string x = s;
+        Console.WriteLine("Hello 500 anh em  " +x);
+        
+        return x;
+    }
+    static string myFoam(string s)
+    {
+        string x = s;
+        Console.WriteLine("Hello CC  " + x);
 
+        return x;
     }
 }
