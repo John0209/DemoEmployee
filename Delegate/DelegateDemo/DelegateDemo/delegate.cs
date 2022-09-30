b58d2cb2e4d6667593e31ae8ab3c7e1d8c6075f1
Tuấn Vũ <113121915+John0209@users.noreply.github.com>
2022-09-30 20:30:40 +0700

    update

---
+++ Delegate/DelegateDemo/DelegateDemo/delegate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo;
+
+class Demo
+{
+    delegate int MyDelegate(string s);
+    static void Main(string[] args)
+    {
+
+    }
+}
