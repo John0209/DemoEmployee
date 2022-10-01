188e2121aacaa77527752211e637a315a994320a
Tuấn Vũ <113121915+John0209@users.noreply.github.com>
2022-10-01 20:56:50 +0700

    update

---
+++ Delegate/DelegateDemo/DelegateDemo/Event.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    delegate void Mydelegate(string name);
+     class Event
+    {
+        static void Main(string[] args)
+        {
+            student st = new student();
+            st.change += St_change;
+            st.Name = "Huy";
+            Console.WriteLine("Name: "+st.Name);
+            st.Name = "Linh";
+            Console.WriteLine("New Name: " + st.Name);
+            Console.ReadLine();
+        }
+
+        private static void St_change(string name)
+        {
+            Console.WriteLine("Change: "+name);
+        }
+    }
+    class student
+    {
+        public event Mydelegate change;
+        private string _name;
+        public string Name {
+            get => _name;
+            set
+            {
+                _name = value;
+                if(change != null)
+                {
+                    change(Name);
+                }
+            }
+        }
+
+    }
+}
---
+++ Delegate/DelegateDemo/DelegateDemo/Event_Donet.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DelegateDemo.student;
+
+namespace DelegateDemo
+{
+     class Event_Donet
+    {
+        static void Main(string[] args)
+        {
+            student st = new student();
+            st.Change += St_Change;
+            st.Name = "number 1";
+            st.Name = "number 2";
+        }
+
+        private static void St_Change(object sender, nameParameter e)
+        {
+            Console.WriteLine("Change Auto "+e.hala);
+        }
+    }
+    class student{
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnChanege(value);
+            }
+        }
+        private event EventHandler<nameParameter> _change;
+        public event EventHandler<nameParameter> Change
+        {
+            add => _change += value;
+            remove => _change += value;
+        }
+        void OnChanege(string x)
+        {
+            if (_change != null)
+            {
+                // _change(this, EventArgs.Empty);
+                _change(this, new nameParameter(x));
+            }
+        }
+        public class nameParameter : EventArgs
+        {
+            public string hala { get; set; }
+            public nameParameter(string s)
+            {
+                hala = s;
+            }
+        }
+    }
+}
