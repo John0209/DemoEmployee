6e236ff36ddee02753dbce4b796339ab3dfdce65
Tuấn Vũ <113121915+John0209@users.noreply.github.com>
2022-10-05 10:51:10 +0700

    update

---
+++ Delegate/DelegateDemo/DelegateDemo/Anomymous.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo;
+
+ class Anomymous
+{
+   delegate void HelloD(int i);
+
+    public static void Main(string[] args)
+    {
+        HelloD dele = delegate (int i) { Console.WriteLine($"Hello {i}"); };
+        dele(1);
+    }
+    static void help(HelloD monkey, int val)
+    {
+        val += 1;
+        monkey(val);
+    }
+}
---
+++ Delegate/DelegateDemo/MultiThreading/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace FptEdu.MultiThreading;
+public class Printer
+{
+    public void PrintNumbers()
+    {
+        Console.WriteLine($"{Thread.CurrentThread.Name} is running...");
+        for(int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"Value is {i}");
+            Thread.Sleep(3000);// cho sleep 1s
+        }
+        Console.WriteLine();
+    }
+    public class program
+    {
+        public static void Main()
+        {
+            //1. lấy tên luồng chính 
+            Thread primaryThread= Thread.CurrentThread;
+            primaryThread.Name = "Primary Thread";
+           // Console.WriteLine($"{primaryThread.Name} is running in Main method");
+            Printer p = new Printer();
+            Thread secondThread = new Thread(new ThreadStart(p.PrintNumbers));
+            secondThread.Name = "TheSecond Thread";
+            secondThread.Start();
+            //2. tip tuc cho threaed chính làm
+            Console.WriteLine($"{Thread.CurrentThread.Name} is running...Hi");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Main Thread: {i}");
+                Thread.Sleep(1000);// cho sleep 1s
+            }
+            Console.WriteLine($"Main thread done!...");
+
+        }
+    }
+}
